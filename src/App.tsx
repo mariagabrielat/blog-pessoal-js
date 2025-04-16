25baab5806d4d9a695d3b1b4c5ceabde127e4e02
mariagabrielat <mariagabrielatech@gmail.com>
2025-04-16 09:00:33 -0300

    preparando para o deploy

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,16 @@ import DeletarTema from './components/temas/deletartema/DeletarTema'
 import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
 import FormPostagem from './components/postagens/formpostagem/FormPostagem'
 import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import Perfil from './pages/perfil/Perfil'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 
 function App() {
   return (
     <>
       <AuthProvider>
+      <ToastContainer />
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -34,6 +38,8 @@ function App() {
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
               <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
+              
             </Routes>
           </div>
           <Footer />
